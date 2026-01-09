1303621ba9e767e273390fa7ad987451494e4c9a
Harry Cheng <f1301110@gmail.com>
2026-01-09 17:00:11 +0800

    feature: roll dice app with randomizer

--- my_app/lib/gradient_container.dart
+++ my_app/lib/gradient_container.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 
 import 'package:my_app/style_text.dart';
+import 'package:my_app/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer({super.key, required this.colors});
+  const GradientContainer({super.key, required this.colors});
 
   final List<Color> colors;
-
-  var activeDiceImage = 'assets/images/dice-1.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-3.png';
-    print('chaging image...');
-  }
 
   @override
   Widget build(context) {
@@ -28,22 +22,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(activeDiceImage, width: 200),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                // padding: const EdgeInsets.only(top: 20),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const StyleText('Roll'),
-            ),
-          ],
-        ),
+        child: DiceRoller(),
       ),
     );
   }
